c89267c9ed24c2733845e61fac17d3e9cc345d0e
Ggamer55 <gilbertodamian14@gmail.com>
2018-12-02 23:32:42 -0600

    Update DateUtil.java

--- src/main/java/net/minebukket/restart/util/DateUtil.java
+++ src/main/java/net/minebukket/restart/util/DateUtil.java
@@ -1,11 +1,3 @@
-/*******************************************************************************
- *  Copyright (C) SparkNetwork - All Rights Reserved
- *   * Unauthorized copying of this file, via any medium is strictly prohibited
- *   * Proprietary and confidential
- *   * Written by Gilberto Garcia <gilbertodamian14@gmail.com>, May 2018
- *
- ******************************************************************************/
-
 package net.minebukket.restart.util;
 
 import java.util.StringJoiner;
