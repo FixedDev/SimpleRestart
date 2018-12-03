31d8f9de257c2f68b150b117c33e235b9eab44c5
Ggamer55 <gilbertodamian14@gmail.com>
2018-12-02 23:35:52 -0600

    Update DateUtilTest.java

--- src/test/java/DateUtilTest.java
+++ src/test/java/DateUtilTest.java
@@ -3,13 +3,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/*******************************************************************************
- *  Copyright (C) SparkNetwork - All Rights Reserved
- *   * Unauthorized copying of this file, via any medium is strictly prohibited
- *   * Proprietary and confidential
- *   * Written by Gilberto Garcia <gilbertodamian14@gmail.com>, May 2018
- *
- ******************************************************************************/
 public class DateUtilTest {
 
     @Test
