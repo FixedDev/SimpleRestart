16e2a4b1d0654629975fcd6350156210d6ce0ab0
Ggamer55 <gilbertodamian14@gmail.com>
2018-12-02 23:33:01 -0600

    Update ServerRestartEvent.java

--- src/main/java/net/minebukket/restart/event/ServerRestartEvent.java
+++ src/main/java/net/minebukket/restart/event/ServerRestartEvent.java
@@ -6,13 +6,6 @@ import org.bukkit.Server;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-/*******************************************************************************
- *  Copyright (C) SparkNetwork - All Rights Reserved
- *   * Unauthorized copying of this file, via any medium is strictly prohibited
- *   * Proprietary and confidential
- *   * Written by Gilberto Garcia <gilbertodamian14@gmail.com>, May 2018
- *
- ******************************************************************************/
 @RequiredArgsConstructor
 public class ServerRestartEvent extends Event {
 
